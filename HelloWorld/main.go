e2d26993115dac5b772e5a9936b80be03e7b9e58
Marcus Bianchi <marcusv.bianchisantos@gmail.com>
2019-08-12 16:49:33 -0300

    correction on files location

---
+++ HelloWorld/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	teste := 10
+	n, _ := fmt.Println("Hello world", teste)
+	fmt.Println(n)
+	for index := 0; index < 10; index++ {
+		fmt.Println(index)
+	}
+
+}
+
+func test() {
+
+}

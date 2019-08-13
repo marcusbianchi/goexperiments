d8a8ac41b6472d46abb02fdf3e3b76a38fc67eec
Marcus Bianchi <marcusv.bianchisantos@gmail.com>
2019-08-13 09:39:16 -0300

    Added exercises for module 1

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -2,17 +2,57 @@ package main
 
 import "fmt"
 
+var x_1 int
+var y_1 string
+var z_1 bool
+
+type new_type int
+
+var n new_type
+
 func main() {
 
-	teste := 10
-	n, _ := fmt.Println("Hello world", teste)
+	//Exercise 1
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Println("---")
+
+	//Exercise 2
+
+	fmt.Println(x_1, y_1, z_1)
+	fmt.Println(x_1)
+	fmt.Println(y_1)
+	fmt.Println(z_1)
+	fmt.Println("---")
+
+	//Exercise 3
+	x_1 = 42
+	y_1 = "James Bond"
+	z_1 = true
+
+	fmt.Println(x_1, y_1, z_1)
+	fmt.Println(x_1)
+	fmt.Println(y_1)
+	fmt.Println(z_1)
+	fmt.Println("---")
+
+	//Exercise 4
 	fmt.Println(n)
-	for index := 0; index < 10; index++ {
-		fmt.Println(index)
-	}
+	fmt.Printf("%T \n", n)
+	n = 42
+	fmt.Println(n)
+	fmt.Println("---")
 
-}
-
-func test() {
+	//Exercise 5
+	x = 5
+	x = int(n)
+	fmt.Println(n)
+	fmt.Println("---")
 
 }

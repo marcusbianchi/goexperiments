1e1f8a56b5f8b2becedc754e74fb2a64d8549c95
Marcus Bianchi <marcusv.bianchisantos@gmail.com>
2019-08-30 11:28:41 -0300

    Commetns and importing

---
+++ Level12/dog/dog.go
@@ -0,0 +1,7 @@
+// Package dog functions for dogs and humans
+package dog
+
+// Years turn humans years in dog years.
+func Years(years int) int {
+	return years * 7
+}
---
+++ Level12/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"./dog"
+)
+
+func main() {
+	fmt.Println(dog.Years(4))
+}
